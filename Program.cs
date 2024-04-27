eb0e2218dd20e78a8a276f9ab8b3be3584bc5305
Maroongy <vladislav.morozov31@mail.ru>
2024-04-27 16:57:36 +0700

    Измененный проект

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        IsLeapYear(2024);  // Вывод: 2024 год високосный
+        IsLeapYear(1900);  // Вывод: 1900 год не високосный
+        IsLeapYear(2000);  // Вывод: 2000 год високосный
+    }
+
+    static void IsLeapYear(int year)
+    {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            Console.WriteLine($"{year} год високосный");
+        }
+        else
+        {
+            Console.WriteLine($"{year} год не високосный");
+        }
+    }
+}
+
